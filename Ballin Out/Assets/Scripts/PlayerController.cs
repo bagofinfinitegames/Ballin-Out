444a8aaf42206bd25328d7d6abafd18625a8dce4
aneylon <arlen.m.neylon@gmail.com>
2018-07-21 13:19:30 -0600

    Finish basic setup.

--- Ballin Out/Assets/Scripts/PlayerController.cs
+++ Ballin Out/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
+    public float speed = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Text countText;
+    public Text gameOverText;
+
+    private int maxCount = 8;
+    private Rigidbody rb;
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        UpdateText();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            //Destroy(other.gameObject);
+            count += 1;
+            UpdateText();
+        }
+    }
+    private void UpdateText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= maxCount)
+        {
+            gameOverText.gameObject.SetActive(true);
+        }
+    }
 }
--- Ballin Out/Assets/Scripts/Spinner.cs
+++ Ballin Out/Assets/Scripts/Spinner.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Spinner : MonoBehaviour {
+    public float speed = 50;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Update()
+    {
+        transform.Rotate(new Vector3(speed, speed, speed) * Time.deltaTime);
+    }
 }
